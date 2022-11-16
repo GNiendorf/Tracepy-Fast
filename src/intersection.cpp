cc08ec58ef6b9885341b1bcf3415d5499d44ae68
GNiendorf <gavinniendorf@gmail.com>
2022-11-16 00:15:08 -0500

    interaction kernel work

--- src/intersection.cpp
+++ src/intersection.cpp
@@ -1,9 +1,76 @@
-#include <alpaka/alpaka.hpp>
+#include "intersection.h"
 
-#include <chrono>
-#include <iostream>
-#include <random>
-#include <typeinfo>
+template<typename TAcc>
+ALPAKA_FN_ACC float conics_func(
+    TAcc const& acc,
+    float Sc,
+    float Skappa,
+    float rho,
+    float Zi)
+{
+    // Conic equation.
+    float func = Zi - Sc*(rho*rho)/(1 + alpaka::math::sqrt(acc, (1-Skappa*(Sc*Sc)*(rho*rho))));
+    return func;
+}
+
+
+class InteractionKernel
+{
+public:
+    ALPAKA_NO_HOST_ACC_WARNING
+    template<typename TAcc, typename TIdx>
+    ALPAKA_FN_ACC auto operator()(
+        TAcc const& acc,
+        float* X,
+        float* Y,
+        float* Z,
+        float* D0,
+        float* D1,
+        float* D2,
+        float Skappa,
+        float Sc,
+        float SDiam,
+        float rayN,
+        float surfaceN,
+        int intertype,
+        TIdx const& nRays) const -> void
+    {
+
+        TIdx const gridThreadIdx(alpaka::getIdx<alpaka::Grid, alpaka::Threads>(acc)[0u]);
+        TIdx const threadElemExtent(alpaka::getWorkDiv<alpaka::Thread, alpaka::Elems>(acc)[0u]);
+        TIdx const threadFirstElemIdx(gridThreadIdx * threadElemExtent);
+
+        if(threadFirstElemIdx < nRays)
+        {
+
+            TIdx const threadLastElemIdx(threadFirstElemIdx + threadElemExtent);
+            TIdx const threadLastElemIdxClipped((nRays > threadLastElemIdx) ? threadLastElemIdx : nRays);
+
+            for(TIdx i(threadFirstElemIdx); i < threadLastElemIdxClipped; ++i)
+            {
+
+                float rho = alpaka::math::sqrt(acc, X[i]*X[i] + Y[i]*Y[i]);
+                float E = Sc / alpaka::math::sqrt(acc, (1-Skappa*(Sc*Sc)*(rho*rho)));
+                float normal[3] = {-X[i]*E, -Y[i]*E, 1.f};
+                float deriv = normal[0]*D0[i] + normal[1]*D1[i] + normal[2]*D2[i];
+                float surf_norm = normal[0]*normal[0]+normal[1]*normal[1]+normal[2]*normal[2];
+
+                float mu = rayN / surfaceN;
+                float a = mu*deriv/surf_norm;
+                float b = (mu*mu-1)/surf_norm;
+                if (intertype == 0) {} 
+                // Needed for total internal reflection even if typeof is refraction.
+                else if (b > pow(a, 2) or intertype == 1) {
+                    D0[i] = D0[i]-2.f*a*normal[0];
+                    D1[i] = D1[i]-2.f*a*normal[1];
+                    D2[i] = D2[i]-2.f*a*normal[2];
+                } else if (intertype == 2) {
+                    //float* D_new = refraction(D, normal, a, b);
+                }
+            }
+        }
+    }
+};
 
 class IntersectionKernel
 {
@@ -15,32 +82,32 @@ public:
         float* X,
         float* Y,
         float* Z,
-        TIdx const& numElements) const -> void
+        float* D0,
+        float* D1,
+        float* D2,
+        float Skappa,
+        float Sc,
+        float SDiam,
+        TIdx const& nRays) const -> void
     {
 
         TIdx const gridThreadIdx(alpaka::getIdx<alpaka::Grid, alpaka::Threads>(acc)[0u]);
         TIdx const threadElemExtent(alpaka::getWorkDiv<alpaka::Thread, alpaka::Elems>(acc)[0u]);
         TIdx const threadFirstElemIdx(gridThreadIdx * threadElemExtent);
 
-        if(threadFirstElemIdx < numElements)
+        if(threadFirstElemIdx < nRays)
         {
-            // Calculate the number of elements to compute in this thread.
-            // The result is uniform for all but the last thread.
+
             TIdx const threadLastElemIdx(threadFirstElemIdx + threadElemExtent);
-            TIdx const threadLastElemIdxClipped((numElements > threadLastElemIdx) ? threadLastElemIdx : numElements);
+            TIdx const threadLastElemIdxClipped((nRays > threadLastElemIdx) ? threadLastElemIdx : nRays);
 
             for(TIdx i(threadFirstElemIdx); i < threadLastElemIdxClipped; ++i)
             {
-                // Hard-coded ray and surface parameters for now.
-                float D[3] = {0.f, 0.f, 1.f};
-                float Skappa = 0.f;
-                float Sc = 0.05f;
-                float SDiam = 10.f;
 
                 // Initial guesses. See Spencer, Murty for explanation
-                float s_0 = -Z[i]/D[2];
-                float X_1 = X[i]+D[1]*s_0;
-                float Y_1 = Y[i]+D[2]*s_0;
+                float s_0 = -Z[i]/D2[i];
+                float X_1 = X[i]+D1[i]*s_0;
+                float Y_1 = Y[i]+D2[i]*s_0;
                 float s_j[2] = {0.f, 0.f};
 
                 //Initial error.
@@ -52,21 +119,23 @@ public:
                 while (error > 1e-6 and n_iter < n_max)
                 {
                     // Iterated coordinates.
-                    Xi = X_1 + D[0]*s_j[0];
-                    Yi = Y_1 + D[1]*s_j[0];
-                    Zi = 0.f + D[2]*s_j[0];
+                    Xi = X_1 + D0[i]*s_j[0];
+                    Yi = Y_1 + D1[i]*s_j[0];
+                    Zi = 0.f + D2[i]*s_j[0];
 
                     float rho = alpaka::math::sqrt(acc, Xi*Xi + Yi*Yi);
 
                     if (rho > SDiam/2.f)
                         printf("Not on surface!: rho:%f, Radius:%f\n", rho, SDiam/2.f);
 
-                    // Conic equation.
-                    float func = Zi - Sc*(rho*rho)/(1 + alpaka::math::sqrt(acc, (1-Skappa*(Sc*Sc)*(rho*rho))));
+                    // Function value and derivative at iterated point.
+                    float func = conics_func(acc, Sc, Skappa, rho, Zi);
+
                     // See Spencer, Murty section on rotational surfaces for definition of E.
                     float E = Sc / alpaka::math::sqrt(acc, (1-Skappa*(Sc*Sc)*(rho*rho)));
                     float normal[3] = {-Xi*E, -Yi*E, 1.f};
-                    float deriv = normal[0]*D[0] + normal[1]*D[1] + normal[2]*D[2];
+
+                    float deriv = normal[0]*D0[i] + normal[1]*D1[i] + normal[2]*D2[i];
 
                     // Newton-raphson method
                     s_j[0] = s_j[1];
@@ -79,14 +148,14 @@ public:
                 }
 
                 // Check prevents rays from propagating backwards.
-                float bcheck = (Xi-X[i])*D[0] + (Yi-Y[i])*D[1] + (Zi-Z[i])*D[2];
+                float bcheck = (Xi-X[i])*D0[i] + (Yi-Y[i])*D1[i] + (Zi-Z[i])*D2[i];
                 if (n_iter == n_max || s_0+s_j[0] < 0.f || bcheck < 0.f)
                 {
                     // Dummy values for now. This implies that the algo did not converge.
                     // Most likely because the ray did not intersect with the surface.
-                    X[i] = -10.f;
-                    Y[i] = -10.f;
-                    Z[i] = -10.f;
+                    X[i] = -9999.f;
+                    Y[i] = -9999.f;
+                    Z[i] = -9999.f;
                 } else {
                     // Algorithm converged. Update position of rays.
                     X[i] = Xi;
@@ -100,37 +169,26 @@ public:
 
 auto main() -> int
 {
-    // Define the index domain
-    using Dim = alpaka::DimInt<1u>;
-    using Idx = std::size_t;
-
-    // Define the accelerator
-    //
     // - AccGpuCudaRt
     // - AccCpuThreads
     // - AccCpuFibers
     // - AccCpuSerial
+    using Data = std::float_t;
+    using Dim = alpaka::DimInt<1u>;
+    using Idx = std::size_t;
     using Acc = alpaka::AccGpuCudaRt<Dim, Idx>;
-    std::cout << "Using alpaka accelerator: " << alpaka::getAccName<Acc>() << std::endl;
-
-    // Defines the synchronization behavior of a queue
-    //
-    // choose between Blocking and NonBlocking
     using QueueProperty = alpaka::Blocking;
     using QueueAcc = alpaka::Queue<Acc, QueueProperty>;
-
-    // Select a device
     auto const devAcc = alpaka::getDevByIdx<Acc>(0u);
+    Idx const elementsPerThread(1u);
 
-    // Create a queue on the device
     QueueAcc queue(devAcc);
 
-    // Define the work division
     int nrays = 100000u;
     Idx const numElements(nrays);
-    Idx const elementsPerThread(8u);
     alpaka::Vec<Dim, Idx> const extent(numElements);
 
+/*
     // Let alpaka calculate good block and grid sizes given our full problem extent
     alpaka::WorkDivMembers<Dim, Idx> const workDiv(alpaka::getValidWorkDiv<Acc>(
         devAcc,
@@ -138,24 +196,32 @@ auto main() -> int
         elementsPerThread,
         false,
         alpaka::GridBlockExtentSubDivRestrictions::Unrestricted));
+*/
 
-    // Define the buffer element type
-    using Data = std::float_t;
+    Idx const blocksPerGrid = 5;
+    Idx const threadsPerBlock = 512;
+    auto workDiv = alpaka::WorkDivMembers<Dim, Idx>{blocksPerGrid, threadsPerBlock, elementsPerThread};
 
     // Get the host device for allocating memory on the host.
     using DevHost = alpaka::DevCpu;
     auto const devHost = alpaka::getDevByIdx<DevHost>(0u);
 
-    // Allocate 3 host memory buffers
     using BufHost = alpaka::Buf<DevHost, Data, Dim, Idx>;
     BufHost bufHostX(alpaka::allocBuf<Data, Idx>(devHost, extent));
     BufHost bufHostY(alpaka::allocBuf<Data, Idx>(devHost, extent));
     BufHost bufHostZ(alpaka::allocBuf<Data, Idx>(devHost, extent));
 
-    // Initialize the host input vectors A and B
+    BufHost bufHostD0(alpaka::allocBuf<Data, Idx>(devHost, extent));
+    BufHost bufHostD1(alpaka::allocBuf<Data, Idx>(devHost, extent));
+    BufHost bufHostD2(alpaka::allocBuf<Data, Idx>(devHost, extent));
+
     Data* pBufHostX(alpaka::getPtrNative(bufHostX));
     Data* pBufHostY(alpaka::getPtrNative(bufHostY));
     Data* pBufHostZ(alpaka::getPtrNative(bufHostZ));
+
+    Data* pBufHostD0(alpaka::getPtrNative(bufHostD0));
+    Data* pBufHostD1(alpaka::getPtrNative(bufHostD1));
+    Data* pBufHostD2(alpaka::getPtrNative(bufHostD2));
 
     // Generate random ray positions between -1 and 1 for each coordinate.
     std::random_device rd{};
@@ -167,21 +233,37 @@ auto main() -> int
         pBufHostX[i] = dist(eng);
         pBufHostY[i] = dist(eng);
         pBufHostZ[i] = dist(eng);
+
+        pBufHostD0[i] = 0.f;
+        pBufHostD1[i] = 0.f;
+        pBufHostD2[i] = 1.f;
     }
 
-    // Allocate 3 buffers on the accelerator
     using BufAcc = alpaka::Buf<Acc, Data, Dim, Idx>;
     BufAcc bufAccX(alpaka::allocBuf<Data, Idx>(devAcc, extent));
     BufAcc bufAccY(alpaka::allocBuf<Data, Idx>(devAcc, extent));
     BufAcc bufAccZ(alpaka::allocBuf<Data, Idx>(devAcc, extent));
+
+    BufAcc bufAccD0(alpaka::allocBuf<Data, Idx>(devAcc, extent));
+    BufAcc bufAccD1(alpaka::allocBuf<Data, Idx>(devAcc, extent));
+    BufAcc bufAccD2(alpaka::allocBuf<Data, Idx>(devAcc, extent));
 
     // Copy Host -> Acc
     alpaka::memcpy(queue, bufAccX, bufHostX);
     alpaka::memcpy(queue, bufAccY, bufHostY);
     alpaka::memcpy(queue, bufAccZ, bufHostZ);
 
+    alpaka::memcpy(queue, bufAccD0, bufHostD0);
+    alpaka::memcpy(queue, bufAccD1, bufHostD1);
+    alpaka::memcpy(queue, bufAccD2, bufHostD2);
+
     // Instantiate the kernel function object
     IntersectionKernel kernel;
+
+    // Hard-coded surface parameters for now.
+    float Skappa = 0.f;
+    float Sc = 0.05f;
+    float SDiam = 10.f;
 
     // Create the kernel execution task.
     auto const taskKernel = alpaka::createTaskKernel<Acc>(
@@ -190,13 +272,21 @@ auto main() -> int
         alpaka::getPtrNative(bufAccX),
         alpaka::getPtrNative(bufAccY),
         alpaka::getPtrNative(bufAccZ),
+        alpaka::getPtrNative(bufAccD0),
+        alpaka::getPtrNative(bufAccD1),
+        alpaka::getPtrNative(bufAccD2),
+        Skappa,
+        Sc,
+        SDiam,
         numElements);
 
     // Enqueue the kernel execution task
     {
         const auto beginT = std::chrono::high_resolution_clock::now();
         alpaka::enqueue(queue, taskKernel);
+
         alpaka::wait(queue); // wait in case we are using an asynchronous queue to time actual kernel runtime
+
         const auto endT = std::chrono::high_resolution_clock::now();
         std::cout << "Time for kernel execution: " << std::chrono::duration<double>(endT - beginT).count() << 's'
                     << std::endl;
